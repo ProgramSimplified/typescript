d2fc691a956b85e0f0ee145c0a2007d18048b896
杨勇 <1994101600@qq.com>
2020-11-02 21:18:14 +0800

    study: 交叉类型

---
+++ src/advancedType/crossType.ts
@@ -0,0 +1,17 @@
+/**
+ * @file 交叉类型
+ * @author Angus Yang
+ * @date 2020/11/2
+ * @description 将多个类型合并为一个类型, 新的类型具有所有特性(并集)
+ */
+
+interface DogInterface {
+    run(): void
+}
+interface CatInterface {
+    jump(): void
+}
+let pet: DogInterface & CatInterface = {
+    run() {},
+    jump() {}
+}

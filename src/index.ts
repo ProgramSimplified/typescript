8c1910c19244b7524e66164a5d736dc8bff7abd6
杨勇 <1994101600@qq.com>
2020-11-16 21:02:37 +0800

    study: typescript-eslint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-class A {
-    a: number = 1
-}
-
-let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 }
-let n = { x, y, ...z }
-
-let hello : string = 'Hello TypeScript'
+const hello : string = 'Hello TypeScript'
 document.querySelectorAll('.app')[0].innerHTML = hello

0380869e5804cc6ad38303c4e8521f60a9be6a24
Angus Yang <1994101600@qq.com>
2021-11-08 17:42:25 +0800

    Update global-lib.d.ts

--- src/project/declareFile/libs/global-lib.d.ts
+++ src/project/declareFile/libs/global-lib.d.ts
@@ -1,3 +1,6 @@
+// ⚠️ 注意! 函数的声明尽量写成声明形式, 而不是表达式:
+// declare const globalLib: (options: globalLib.Options) => void;
+// 因为函数才有重载, 所以写成表达式形式, 就不能使用同名的命名空间了.
 declare function globalLib(options: globalLib.Options): void;
 
 declare namespace globalLib {

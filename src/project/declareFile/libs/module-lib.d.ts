fba2cbb292e7ed1632b14f66847da9d0bf1731ec
Angus Yang <1994101600@qq.com>
2021-11-08 17:41:57 +0800

    Update module-lib.d.ts

--- src/project/declareFile/libs/module-lib.d.ts
+++ src/project/declareFile/libs/module-lib.d.ts
@@ -1,3 +1,6 @@
+// ⚠️ 注意! 函数的声明尽量写成声明形式, 而不是表达式:
+// declare const moduleLib: (options: Options) => void;
+// 因为函数才有重载, 所以写成表达式形式, 就不能使用同名的命名空间了.
 declare function moduleLib(options: Options): void;
 
 interface Options {
